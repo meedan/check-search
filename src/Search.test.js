c55fef2696778083005fa17f5d6916b928a34fd8
Darius Kazemi <darius@meedan.com>
2021-03-26 14:38:08 -0700

    Linting

--- src/Search.test.js
+++ src/Search.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Search from './Search';
 import { mountWithIntl } from './helpers/intl-enzyme-test-helper';
+
 describe('<Search />', () => {
   it('renders the basic filter', () => {
     const wrapper = mountWithIntl(<Search locale="en" />);
