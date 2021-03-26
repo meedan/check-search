95e49e76fbf9a927c24ed6cb643ad9e896b9e90b
Darius Kazemi <darius@meedan.com>
2021-03-25 17:05:55 -0700

    Basic search functionality
    This commit triggers a call to API v2 when the search button is pressed. It uses the `client` instance from `jsonapi-react` for manual API requests, documented here:
    
    https://github.com/aribouius/jsonapi-react#manual-requests

--- src/Search.js
+++ src/Search.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 import {
   makeStyles,
@@ -8,8 +8,11 @@ import {
   TextField,
   Grid,
   InputAdornment,
+  Card,
+  CardContent,
 } from '@material-ui/core';
 import { Search as SearchIcon } from '@material-ui/icons';
+import { useClient } from 'jsonapi-react';
 import messages from '../localization/messages';
 
 const useStyles = makeStyles((theme) => ({
@@ -20,11 +23,55 @@ const useStyles = makeStyles((theme) => ({
   searchButton: {
     margin: theme.spacing(1),
   },
+  results: {
+    margin: theme.spacing(2),
+  },
 }));
 
 function Search(props) {
   const classes = useStyles();
   const { locale } = props;
+  const [searchText, setSearchText] = useState('');
+  const [results, setResults] = useState([]);
+  const client = useClient();
+
+  async function getData() {
+    const { data } = await client.fetch([
+      'items',
+      {
+        filter: {
+          similar_to_text: searchText,
+          similarity_threshold: 0.1,
+        },
+      },
+    ]);
+
+    return data;
+  }
+
+  async function handleSearch() {
+    setResults(await getData());
+  }
+
+  function SearchResults() {
+    return (
+      <>
+        {results.map((item) => (
+          <Grid item xs={12} key={item.id} className={classes.results}>
+            <Card variant="outlined">
+              <CardContent>
+                <Typography className={classes.title} variant="h6">
+                  Report Title
+                </Typography>
+                <Typography>{item.title}</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </>
+    );
+  }
+
   return (
     <main className={classes.content}>
       <Toolbar />
@@ -47,16 +94,19 @@ function Search(props) {
                 </InputAdornment>
               ),
             }}
+            onChange={(e) => setSearchText(e.target.value)}
           />
           <Button
             align="left"
             variant="contained"
             color="primary"
             className={classes.searchButton}
+            onClick={handleSearch}
           >
             <FormattedMessage id="search.action" />
           </Button>
         </Grid>
+        <SearchResults />
       </Grid>
     </main>
   );
