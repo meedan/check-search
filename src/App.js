2284afd636a26769edc1352771c806b285a09951
Darius Kazemi <darius.kazemi@gmail.com>
2021-04-06 10:14:01 -0700

    Refactoring localization
    We now detect the language using the `window.navigator` object, and webpack now splits the translations into their own resources that are loaded as needed per detected language. Language direction is now detected, too.
    
    Note: this uses `React.Suspend`, which is an experimental feature, but in my opinion can be used in the exact case here, where we are simply delaying rendering until a Promise gets resolved (hence the return of a `throw`, which interacts with the `ErrorBoundary` of `React.Suspend`).

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ import {
   Toolbar,
   Typography,
   CssBaseline,
+  CircularProgress,
 } from '@material-ui/core';
 import { create } from 'jss';
 import rtl from 'jss-rtl';
 import { StylesProvider, jssPreset } from '@material-ui/core/styles';
-import messages from '../localization/messages';
+import rtlDetect from 'rtl-detect';
 import Sidebar from './Sidebar';
 import Search from './Search';
+import locales from '../localization/locales';
 
 const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
 
@@ -25,12 +27,64 @@ const useStyles = makeStyles((theme) => ({
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
   },
+  loading: {
+    display: 'flex',
+    justifyContent: 'center',
+    padding: theme.spacing(3),
+  },
 }));
+
+const messagesCache = {};
+
+// detect locale from browser
+let locale =
+  navigator.languages || navigator.language || navigator.userLanguage || 'en';
+if (locale.constructor === Array) {
+  [locale] = locale;
+}
+locale = locale.replace(/[-_].*$/, '');
+if (locales.indexOf(locale) === -1) {
+  locale = 'en';
+}
+
+const isRtl = rtlDetect.isRtlLang(locale);
+const direction = isRtl ? 'rtl' : 'ltr';
+
+// based on https://stackoverflow.com/a/58617341/4869657
+function AsyncIntlProvider({ children }) {
+  const messages = getMessages(locale);
+
+  return (
+    <div dir={direction}>
+      <IntlProvider locale={locale} messages={messages}>
+        {children}
+      </IntlProvider>
+    </div>
+  );
+}
+
+function getMessages(locale) {
+  if (messagesCache[locale]) {
+    return messagesCache[locale];
+  }
+  throw loadMessages(locale);
+}
+
+async function loadMessages(locale) {
+  // Webpack will intrepret the following import as dynamic loading and
+  // split the locales into chunks
+  const messages = await import(`../localization/${locale}.json`);
+  // Transform message from transifex structured JSON to react-intl
+  const messagesTransformed = {};
+  for (const key of Object.keys(messages.default)) {
+    messagesTransformed[key] = messages[key].string;
+  }
+  messagesCache[locale] = messagesTransformed;
+  return messages;
+}
 
 function App() {
   const classes = useStyles();
-  const locale = 'en';
-  const direction = 'ltr';
 
   const muiTheme = createMuiTheme({
     palette: {
@@ -42,27 +96,35 @@ function App() {
   });
 
   return (
-    <IntlProvider locale={locale} messages={messages[locale]}>
-      <StylesProvider jss={jss}>
-        <ThemeProvider theme={muiTheme}>
-          <div dir={direction} className={classes.root}>
-            <CssBaseline />
-            <AppBar position="fixed" className={classes.appBar}>
-              <Toolbar>
-                <Typography variant="h6" noWrap>
-                  <FormattedMessage
-                    id="homepage.title"
-                    defaultMessage="Similarity Search Prototype"
-                  />
-                </Typography>
-              </Toolbar>
-            </AppBar>
-            <Sidebar />
-            <Search />
-          </div>
-        </ThemeProvider>
-      </StylesProvider>
-    </IntlProvider>
+    <React.Suspense
+      fallback={
+        <div className={classes.loading}>
+          <CircularProgress />
+        </div>
+      }
+    >
+      <AsyncIntlProvider>
+        <StylesProvider jss={jss}>
+          <ThemeProvider theme={muiTheme}>
+            <div className={classes.root}>
+              <CssBaseline />
+              <AppBar position="fixed" className={classes.appBar}>
+                <Toolbar>
+                  <Typography variant="h6" noWrap>
+                    <FormattedMessage
+                      id="homepage.title"
+                      defaultMessage="Similarity Search Prototype"
+                    />
+                  </Typography>
+                </Toolbar>
+              </AppBar>
+              <Sidebar />
+              <Search />
+            </div>
+          </ThemeProvider>
+        </StylesProvider>
+      </AsyncIntlProvider>
+    </React.Suspense>
   );
 }
 
