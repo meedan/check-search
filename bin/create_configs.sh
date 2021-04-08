b7626d93e744df3bc79cf2e652d1e6eea900f2ce
Martin Peck <martin@meedan.com>
2021-04-07 17:38:32 -0700

    Remove use of DEPLOYDIR.

--- bin/create_configs.sh
+++ bin/create_configs.sh
@@ -4,8 +4,8 @@
 # using values from the SSM parameter store.
 
 # The following environment variables must be set:
-if [[ -z ${DEPLOY_ENV+x} || -z ${DEPLOYDIR+x} || -z ${AWS_DEFAULT_REGION+x} ]]; then
-  echo "DEPLOY_ENV, DEPLOYDIR, and AWS_DEFAULT_REGION must be in the environment. Exiting."
+if [[ -z ${DEPLOY_ENV+x} || -z ${AWS_DEFAULT_REGION+x} ]]; then
+  echo "DEPLOY_ENV and AWS_DEFAULT_REGION must be in the environment. Exiting."
   exit 1
 fi
 
@@ -13,7 +13,7 @@ SSM_PREFIX="/${DEPLOY_ENV}/check-search"
 WORKTMP=$(mktemp)
 
 # Create user config.js from SSM parameter value:
-DESTFILE="${DEPLOYDIR}/config.js"
+DESTFILE="config.js"
 aws ssm get-parameters --region $AWS_DEFAULT_REGION --name "${SSM_PREFIX}/config" | jq .Parameters[].Value|sed 's/["]//g' | python -m base64 -d > $WORKTMP
 if (( $? != 0 )); then
   echo "Error retrieving SSM parameter ${SSM_PREFIX}/config. Exiting."
