9d98ca8ee426ffb2c353ad0e0b3e8d3538305b72
Darius Kazemi <darius.kazemi@gmail.com>
2021-04-08 08:21:18 -0700

    Webpack perf and CI integration (#2)
    * Split out a vendor bundle for Webpack
    
    This means that as long as vendor libraries don't change, the client will cache the libraries, only needing to load new application code when it changes.
    
    * Externalize config.js
    
    The `config.js` is now copied into `dist/` on build and included as an external `<script>` resource in `index.html`. The `externals` directive in the webpack config tells webpack that the `import` of `'config'` in `src/index.js` should simply expect the `config` variable to already be loaded in the application through the `<script>` tag.
    
    * Speed up webpack build
    
    Previously `babel-loader` was accidentally parsing in `node_modules` on build. This is no longer the case, and build times have improved by 50%.
    
    * Slack integration in CI

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,21 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const config = require('./config');
+const CopyPlugin = require('copy-webpack-plugin');
+const NODE_ENV = process.env.NODE_ENV || 'production';
+const BUNDLE_PREFIX = process.env.BUNDLE_PREFIX
+  ? `.${process.env.BUNDLE_PREFIX}`
+  : '';
+const nodeModulesPrefix = path.resolve(__dirname, 'node_modules') + '/';
 
 module.exports = {
   entry: './src/index.js',
+  target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js',
   },
   devServer: {
-    port: config.selfHostPort,
+    port: 8001,
     host: '0.0.0.0',
     disableHostCheck: true,
   },
@@ -18,6 +24,7 @@ module.exports = {
       {
         test: /\.(js)$/,
         use: 'babel-loader',
+        exclude: /(node_modules)/,
       },
       {
         test: /\.css$/,
@@ -25,13 +32,35 @@ module.exports = {
       },
     ],
   },
-  mode: 'development',
+  mode: 'production',
+  optimization: {
+    splitChunks: {
+      name: false,
+      cacheGroups: {
+        vendor: {
+          name: 'vendor',
+          filename: `vendor.bundle${BUNDLE_PREFIX}.js`,
+          chunks: 'all',
+          enforce: true,
+          test({ resource }) {
+            return resource && resource.startsWith(nodeModulesPrefix);
+          },
+        },
+      },
+    },
+  },
   resolve: {
     symlinks: true,
+  },
+  externals: {
+    config: 'config',
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: './config.js', to: 'config.js' }],
     }),
   ],
 };
