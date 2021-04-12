87f49ba61bd781effc7a8b90dc0021b131dea476
Martin Peck <51542678+sonoransun@users.noreply.github.com>
2021-04-12 11:46:05 -0700

    Perform deployments to QA and Live via GitLab-CI (#3)
    * Initial support for QA and Live deployments.
    
    * Try using one Dockerfile with docker-entrypoint.sh configuring app via SSM if DEPLOY_ENV is qa or live.
    
    * Build and deploy to QA for this branch.
    
    * No need to copy sample config in build stage.
    
    * Reduce deploy timeout to 30 minutes.
    
    * The terraform created containers for check-search do not append -c.
    
    * Update version of ecs deploy.
    
    * Remove use of DEPLOYDIR.
    
    * Set DEPLOY_ENV and AWS_DEFAULT_REGION when deploying.
    
    * Add dependencies in build for use in SSM configuration of service.
    
    * Install awscli manually during build. Do not use outdated upstream version.
    
    * Remove builds for this branch in preparation for merge.
    
    Co-authored-by: Martin Peck <martin@meedan.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,7 +9,19 @@ if [[ -z ${DEPLOY_ENV+x} ]]; then
     echo "DEPLOY_ENV environment variable must be set. Exiting."
     exit 1
 fi
-cp config.js.example config.js
+
+if [[ "$DEPLOY_ENV" == "qa" || "$DEPLOY_ENV" == "live" ]]; then
+    # Always put config into place when starting service. This is because the
+    # build stage includes the sample configs which real deployments must replace.
+    ./bin/create_configs.sh
+    if (( $? != 0 )); then
+        echo "Error creating configuration files. Exiting."
+        exit 1
+    fi
+else
+    cp config.js.example config.js
+fi
+
 npm install
 # npm run test:lint
 # npm run test
