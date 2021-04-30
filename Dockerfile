52acde6fbc6a92fb45fecbdcbe0f1845149b2510
Martin Peck <51542678+sonoransun@users.noreply.github.com>
2021-04-30 09:08:09 -0700

    Deploy fix to use production /dist build of check-search (#7)
    * For QA and Live deployments build a production version of check-search.
    
    * Command is "npm run build".
    
    * For production mode, serve content via nginx.
    
    * Run nginx in foreground so task is happy.
    
    * Run build at right stage, clean up Dockerfile.
    
    * Pass install flags if set.
    
    * Add webpack
    
    * Try something simpler.
    
    * More debugging.
    
    * Do pass INSTALL_ARGS.
    
    * More debugging.
    
    * Remove debug sleep, and route access and error logs to STDOUT for CloudWatch.
    
    * Log when nginx is started.
    
    * Remove builds for this branch in preparation for merge.
    
    Co-authored-by: Martin Peck <martin@meedan.com>

--- Dockerfile
+++ Dockerfile
@@ -12,7 +12,7 @@ RUN groupadd -r search
 RUN useradd -ms /bin/bash -g search search
 RUN chown search:search .
 
-RUN apt-get update || : && apt-get install -y python python-pip jq unzip curl git
+RUN apt-get update || : && apt-get install -y python python-pip jq unzip curl git nginx
 
 # Due to the outdated version of awscli in upstream repos, manually install latest from Amazon.
 RUN curl --silent --show-error --fail "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && \
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -22,7 +22,30 @@ else
     cp config.js.example config.js
 fi
 
-npm install
-# npm run test:lint
-# npm run test
-npm run start
+
+# NOTE: for a production environment (QA, Live) we serve content
+# via separate web server (nginx). Otherwise, run in development
+# mode directly.
+#
+if [[ "$DEPLOY_ENV" == "qa" || "$DEPLOY_ENV" == "live" ]]; then
+  # Production entrypoint
+  #
+  npm install
+  npm run build
+  mv /var/www/html /var/www/dist-html
+  ln -s /app/dist /var/www/html
+  cat /etc/nginx/sites-available/default | sed 's/listen 80 default_server/listen 8001 default_server/' > /tmp/.tmpf
+  cat /tmp/.tmpf > /etc/nginx/sites-available/default
+  cat /etc/nginx/nginx.conf|sed 's/access_log .var.log.nginx.access.log/access_log \/dev\/stdout/'| \
+      sed 's/error_log .var.log.nginx.error.log/error_log \/dev\/stdout/' > /tmp/.tmpf
+  cat /tmp/.tmpf > /etc/nginx/nginx.conf
+  echo "Serving content via nginx..."
+  nginx -g 'daemon off;'
+else
+  # Developer entrypoint:
+  #
+  npm install
+  # npm run test:lint
+  # npm run test
+  npm run start
+fi
