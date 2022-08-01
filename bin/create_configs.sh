6ae507d7fd5b7eb380e7d06bb6489ac560e7cac8
Martin Peck <martin@meedan.com>
2022-08-01 13:55:43 -0400

    Create configs from ENV rather than using awscli to pull from SSM directly.

--- bin/create_configs.sh
+++ bin/create_configs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This script generates a json configuration file for check-search
-# using values from the SSM parameter store.
+# using values from the environment set via SSM parameter store.
 
 # The following environment variables must be set:
 if [[ -z ${DEPLOY_ENV+x} || -z ${AWS_DEFAULT_REGION+x} ]]; then
@@ -14,7 +14,7 @@ WORKTMP=$(mktemp)
 
 # Create user config.js from SSM parameter value:
 DESTFILE="config.js"
-aws ssm get-parameters --region $AWS_DEFAULT_REGION --name "${SSM_PREFIX}/config" | jq .Parameters[].Value|sed 's/["]//g' | python -m base64 -d > $WORKTMP
+echo $config | python -m base64 -d > $WORKTMP
 if (( $? != 0 )); then
   echo "Error retrieving SSM parameter ${SSM_PREFIX}/config. Exiting."
   exit 1
